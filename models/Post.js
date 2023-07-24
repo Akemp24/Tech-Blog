80a15f0101435013417cd8b9a5246b9f9b436b86
Asiak <asiakemp24@gmail.com>
2023-07-24 18:29:57 -0400

    added comments to the Post model

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,11 @@
+// require the needed packages/files
 const { Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 
+// creating a class called Post from the Model class
 class Post extends Model {}
 
+// initializing Post with id, title, and user_id
 Post.init(
     {
         id: {
@@ -24,6 +27,7 @@ Post.init(
         }
     },
     {
+        // sequelize data
         sequelize,
         freezeTableName: true,
         underscored: true,
@@ -31,4 +35,5 @@ Post.init(
     }
 );
 
+// export the Post class
 module.exports = Post;
