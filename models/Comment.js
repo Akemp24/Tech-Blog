2fc114982fc6965178ace8a16c7c3c5b4d7fba7c
Asiak <asiakemp24@gmail.com>
2023-07-24 18:25:00 -0400

    added comments to the Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,8 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+// creating the comment class that is based of off Model
 
 class Comment extends Model {}
-
+// creating a id, comment, and user_id for the comment class
 Comment.init({
     id: {
         type: DataTypes.INTEGER,
@@ -32,10 +33,12 @@ Comment.init({
         }
     }
 }, {
+    // sequilizing data
     sequelize,
     freezeTableName: true,
     underscored: true,
     modelName: 'comment'
 });
 
+// exporting the comment
 module.exports = Comment;
